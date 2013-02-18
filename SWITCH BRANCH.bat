adf1d4b7dadfdc7259620f2bd4bd7045b70790b1
Marius <marius_flagstad@hotmail.com>
2013-02-19 00:20:34 +0100

    Switch branch changes

--- SWITCH BRANCH.bat
+++ SWITCH BRANCH.bat
@@ -5,6 +5,6 @@ timeout -1
 set INPUT=
 set /P INPUT=Write in the branch you want to switch to: %=%
 git checkout "%INPUT%"
-echo "Branch switched. Do you want to get latest files? If not close window."
-timeout -1
 git pull
+echo "Branch switched"
+timeout -1
